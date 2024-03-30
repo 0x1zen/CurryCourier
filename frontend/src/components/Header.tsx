e7203b71076b71ac8403b4d04057f4b15f945cdd
0x1zen <rajdubalwork@gmail.com>
2024-03-30 13:48:04 +0530

    Added footer component

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -6,7 +6,8 @@ const Header = () => {
   return (
     <div className="border-b-2 border-b-orange-500 py-6">
         <div className="container mx-auto flex justify-between items-center">
-            <Link to="/" className="text-3xl font-bold tracking-tight text-orange-500">
+            <Link to="/" className="text-3xl font-bold tracking-tight text-orange-500 flex">
+            <img src="https://cdn.discordapp.com/attachments/1223496785373433977/1223542280686932048/isolated_hamburger_with_splash_ink_background-removebg-preview.png?ex=661a3b8c&is=6607c68c&hm=ab13fd5d497c0e7c3e6d0c75eb1975593bd48659efa1fb2a5b500542936d17f9&" className="w-10 h-10" />
                 CurryCourier.com    
             </Link>
         <div className="md:hidden">
--- frontend/src/components/pages/HomePage.tsx
+++ frontend/src/components/pages/HomePage.tsx
@@ -7,6 +7,18 @@ const HomePage = () => {
             </h1>
             <span className="text-xl"> Food is just a click away!</span>
         </div>
+        <div className="grid md:grid-cols-2 gap-5">
+          <img src="https://cdn.discordapp.com/attachments/1223496785373433977/1223507581104947280/landing.png?ex=661a1b3b&is=6607a63b&hm=f2e16d19dfcda04347fc24e5f5d5f2621779b7f41e444c46fd0c5537aa03a9ea&"/>
+          <div className="flex flex-col items-center justify-center gap-4 text-center">
+            <span className="font-bold text-3xl tracking-tighter">
+              Order Takeaway Even Faster!
+            </span>
+            <span>
+              Download CurryCourier App for convinient ordering and personalized recommendations.
+            </span>
+            <img src="https://cdn.discordapp.com/attachments/1223496785373433977/1223507754115792986/appDownload.png?ex=661a1b64&is=6607a664&hm=a7759dcabf45c8a00d4326dd73c55191c98242e15e245089b043173cf3a22a2a&"/>
+          </div>
+        </div>
     </div>
   )
 }
